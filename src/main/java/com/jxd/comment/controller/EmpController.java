7a0969adbf6f59a234f301af4cec46e8d1926b0a
18904306272 <932564901@qq.com>
2020-10-28 16:01:01 +0800

    1.0

--- src/main/java/com/jxd/comment/controller/EmpController.java
+++ src/main/java/com/jxd/comment/controller/EmpController.java
@@ -5,6 +5,7 @@ import com.jxd.comment.service.IEmpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -21,9 +22,9 @@ public class EmpController {
     @Autowired
     private IEmpService empService;
 
-    @RequestMapping("/showInfo")
+    @RequestMapping("/showInfo/{empno}")
     @ResponseBody
-    public EmpJxd showInfo(int empno) {
+    public EmpJxd showInfo(@PathVariable("empno") int empno) {
         return empService.selectOwn(empno);
     }
 

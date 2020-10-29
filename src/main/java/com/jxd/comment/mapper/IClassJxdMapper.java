17735f035672332163ed3f7780d40ed9a0469b90
gh <1797098150@qq.com>
2020-10-29 15:23:32 +0800

    10/29 15:23

--- src/main/java/com/jxd/comment/mapper/IClassJxdMapper.java
+++ src/main/java/com/jxd/comment/mapper/IClassJxdMapper.java
@@ -3,5 +3,8 @@ package com.jxd.comment.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.jxd.comment.model.ClassJxd;
 
+import java.util.List;
+
 public interface IClassJxdMapper extends BaseMapper<ClassJxd>{
+    List<ClassJxd> getClassByTeacher(int empno);
 }
